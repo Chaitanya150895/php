bb788c2c62310303b911a021089c16a4d536085d
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-11 18:47:24 +0530

    var_dump showing the size of the array and int value

--- test.php
+++ test.php
@@ -5,9 +5,8 @@
 <h1>My first PHP page</h1>
 
 <?php
-$x =['asdfghj'=>83797]; //array and it's value at the firat position
-echo json_encode ($x); //Associative array converted into json
-//var_dump($x);
+$x =['asdfghj'=>83797];
+var_dump($x);
 ?>
 
 </body>
