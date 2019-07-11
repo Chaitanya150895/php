5d01113009e213e1ddad3737a06e2e2d743626ec
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-11 16:34:06 +0530

    performing operation and printing it in the same

--- test.php
+++ test.php
@@ -5,12 +5,13 @@
 <h1>My first PHP page</h1>
 
 <?php
+$x=5;
+$y=4;
 
-$txt = "PHP";
+echo $x.'<br>'; 
+echo $y .'<br>';
 
-
-echo "I LOVE " .$txt. "!";
-
+echo "Total is " .($x + $y);
 ?>
 
 </body>
