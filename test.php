9a63470d3aa30d89340f7f33f0e8044fa24263e6
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-11 16:54:12 +0530

    The global keyword is used to access a global variable from within a function.
    To do this, use the global keyword before the variables (inside the function)

--- test.php
+++ test.php
@@ -5,14 +5,20 @@
 <h1>My first PHP page</h1>
 
 <?php
-function myTest() {
-    $x = 5; // local scope
-    echo "<p>Variable x inside function is: $x</p>";
-} 
-myTest();
+$x=5;
+$y=10;
 
-// using x outside the function will generate an error
-echo "<p>Variable x outside function is: $x</p>";
+function mytest(){
+    global $x, $y;
+    $y = $x + $y;
+
+    
+}
+echo $y.'<br>';
+
+
+mytest();
+echo $y; 
 ?>
 
 </body>
