8b0395ed9a6d65ac12abaf81fcbefa7f19f41d98
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-11 19:33:15 +0530

    str_word_count() function counts the number of words in a string:

--- test.php
+++ test.php
@@ -3,10 +3,9 @@
 <body>
 
 <h1>My first PHP page</h1>
-<p>To Get The Length of a String<br>
-The PHP strlen() function returns the length of a string.</p>
+
 <?php
-echo strlen("Hello World"); //
+echo str_word_count("Hello World"); //
 ?>
 
 </body>
