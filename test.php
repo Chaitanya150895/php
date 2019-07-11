a77c0641ff71c608865ceca99232227711317ac1
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-11 19:25:20 +0530

    we use the class keyword. A class is a structure that can contain properties and methods

--- test.php
+++ test.php
@@ -5,8 +5,17 @@
 <h1>My first PHP page</h1>
 
 <?php
-$cars = array ("Volvo", "BMW", "Toyota");
-var_dump($cars);
+class Car {                         //class name Car
+    function Car () {               //constructor named Car
+        $this->model = "VW";        //in php it used pointer operator instead of . (this.) and assigned the string value to it
+    }
+}
+
+//creating an object
+$herbie = new Car();                //created and new object named car of the same name as car and accessed it's property
+
+//show object properties
+echo $herbie->model;                //used echo to print the value and pointer to access the model's value
 ?>
 
 </body>
