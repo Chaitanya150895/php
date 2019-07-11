4ad9c745a34ff964343c4e9b1ec09e7845b837a8
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-11 17:16:43 +0530

    to output text with the echo command

--- test.php
+++ test.php
@@ -5,15 +5,10 @@
 <h1>My first PHP page</h1>
 
 <?php
-$x=5;
-$y=10;
-
-function myTest(){
-    $GLOBALS['y'] = $GLOBALS['x'] + $GLOBALS['y'];   
-}
-
-myTest();
-echo $y; 
+echo "<h2>PHP is Fun!</h2>";
+echo "Hello world!<br>";
+echo "I'm about to learn PHP!<br>";
+echo "This ", "string ", "was ", "made ", "with multiple parameters.";
 ?>
 
 </body>
