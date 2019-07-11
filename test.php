3128177e2643d3715f29efbfc43f93dcb71f555d
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-11 19:32:21 +0530

    strlen() function returns the length of a string.

--- test.php
+++ test.php
@@ -3,11 +3,10 @@
 <body>
 
 <h1>My first PHP page</h1>
-
+<p>To Get The Length of a String<br>
+The PHP strlen() function returns the length of a string.</p>
 <?php
-$x = "Hello world!";
-$x = null;
-var_dump($x);
+echo strlen("Hello World"); //
 ?>
 
 </body>
