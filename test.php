fd015e50b32976906de44870f8617e1608f100f7
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-11 16:43:36 +0530

    A variable declared within a function has a LOCAL SCOPE and can only be accessed within that function

--- test.php
+++ test.php
@@ -5,16 +5,14 @@
 <h1>My first PHP page</h1>
 
 <?php
-$x=5; //global scope
-
-function myTest(){
-    //using x inside this function will generate this error
-    echo "<p>Variable c inside function is: $x </p>";
-}
-
+function myTest() {
+    $x = 5; // local scope
+    echo "<p>Variable x inside function is: $x</p>";
+} 
 myTest();
 
-echo "<p>Variable x outside function is : $x</p>"
+// using x outside the function will generate an error
+echo "<p>Variable x outside function is: $x</p>";
 ?>
 
 </body>
