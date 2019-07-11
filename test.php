e7b7141bb6dbfa4d0b3b0af891de9541231d6afa
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-11 19:28:35 +0530

    the Variable is emptied by setting the value to NULL, printed using var_dumped

--- test.php
+++ test.php
@@ -5,17 +5,9 @@
 <h1>My first PHP page</h1>
 
 <?php
-class Car {                         //class name Car
-    function Car () {               //constructor named Car
-        $this->model = "VW";        //in php it used pointer operator instead of . (this.) and assigned the string value to it
-    }
-}
-
-//creating an object
-$herbie = new Car();                //created and new object named car of the same name as car and accessed it's property
-
-//show object properties
-echo $herbie->model;                //used echo to print the value and pointer to access the model's value
+$x = "Hello world!";
+$x = null;
+var_dump($x);
 ?>
 
 </body>
