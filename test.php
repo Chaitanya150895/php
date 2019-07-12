a102cc1b70cbdab0d688330bd560b5f90c44a632
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-12 11:42:52 +0530

    >	Greater than	$x > $y	Returns true if $x is greater than $y

--- test.php
+++ test.php
@@ -6,10 +6,9 @@
 
 <?php
 $x = 100;
-$y = 100;
+$y = 50;
 
-var_dump($x !== $y); // returns true because types are not equal
-
+var_dump($x > $y); // returns true because $x is greater than $y
 
 ?>
 </body>
