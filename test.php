c3dd813ded203d949c74b98750de6ac961de2324
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-12 14:01:10 +0530

    output "Have a good morning!" if the current time is less than 10, and "Have a good day!" if the current time is less than 20. Otherwise it will output "Have a good night!"

--- test.php
+++ test.php
@@ -8,7 +8,9 @@
 
 $t = date("H");
 
-if ($t <"20"){
+if ($t < "10") {
+    echo "Have a good morning!";
+} elseif ($t < "20") {
     echo "Have a good day!";
 } else {
     echo "Have a good night!";
