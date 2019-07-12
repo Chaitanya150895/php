13149b0a35f05655d36c5681c07ba9c67a380038
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-12 16:20:23 +0530

    Associative arrays are arrays that use named keys that you assign to them

--- test.php
+++ test.php
@@ -3,16 +3,13 @@
 <body>
 
 <h1>My first PHP page</h1>
-
+<p>PHP Associative Arrays</p>
 <?php
 
-$cars = array("Volvo", "BMW", "Toyota");
-$arrlength = count ($cars);
-
-for($x = 0; $x < $arrlength; $x++){
-    echo $cars [$x];
-    echo "<br>";
-}
+$age = array ("Peter" => "35", "Ben" => "37", "Joe" => "48");
+echo "Peter is" .$age['Peter']. "years old." .'<br>';
+echo "Ben is" .$age['Ben']. "years old." .'<br>';
+echo "Joe is" .$age['Joe']. "years old.";
 
 ?>
 </body>
