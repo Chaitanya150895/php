483f1e3ad4caf3a8fe0677ddb9ad75461a2514c4
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-12 15:55:56 +0530

    how to use a default parameter. If we call the function setHeight() without arguments it takes the default value as argument

--- test.php
+++ test.php
@@ -5,15 +5,14 @@
 <h1>My first PHP page</h1>
 
 <?php
-function familyName($fname, $year) {
-    echo "$fname Refsnes, Born in $year <br>";
+function setHeight($minheight = 50) {
+    echo "The height is : $minheight <br>";
 }
 
-familyName("Jani", "1945");
-familyName("Hege", 1998);
-familyName("Stale", "1979");
-familyName("Kai Jim", "1994");
-familyName("Borge", "1988");
+setHeight(350);
+setHeight(); // will use the default value of 50
+setHeight(135);
+setHeight(80);
 ?>
 </body>
 </html>
