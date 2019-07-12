138a668599808913ac5d29cfecd5e1c5409e31e1
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-12 12:12:34 +0530

    ==	Equality	$x == $y	Returns true if $x and $y have the same key/value pairs

--- test.php
+++ test.php
@@ -5,12 +5,10 @@
 <h1>My first PHP page</h1>
 
 <?php
-
 $x = array ("a" => "red,", "b" => "green,");
 $y = array ("c" => "blue,", "d" => "yellow");
 
-print_r($x + $y); //union of $x and $y
-
+var_dump($x == $y);
 ?>
 </body>
 </html>
