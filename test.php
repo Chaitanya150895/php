ae00613cd7af710057d3315fdf4ff90b24e0a34b
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-12 11:44:56 +0530

    >=	Greater than or equal to	$x >= $y	Returns true if $x is greater than or equal to $y

--- test.php
+++ test.php
@@ -5,11 +5,10 @@
 <h1>My first PHP page</h1>
 
 <?php
-$x = 10;
+$x = 50;
 $y = 50;
 
-var_dump($x < $y); // returns true because $x is less than $y
-
+var_dump($x >= $y); // returns true because $x is greater than or equal to $y
 ?>
 </body>
 </html>
