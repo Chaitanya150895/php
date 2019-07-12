33cab6d569ed6f985998d97f6b665c8678678dcb
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-12 16:57:51 +0530

    $_SERVER is a PHP super global variable which holds information about headers, paths, and script locations

--- test.php
+++ test.php
@@ -3,16 +3,18 @@
 <body>
 
 <h1>My first PHP page</h1>
-<?php
-$x = 75; 
-$y = 25;
- 
-function addition() { 
-    $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y']; 
-}
- 
-addition(); 
-echo $z; 
+<?php 
+echo $_SERVER['PHP_SELF'];
+echo "<br>";
+echo $_SERVER['SERVER_NAME'];
+echo "<br>";
+echo $_SERVER['HTTP_HOST'];
+echo "<br>";
+echo $_SERVER['HTTP_REFERER'];
+echo "<br>";
+echo $_SERVER['HTTP_USER_AGENT'];
+echo "<br>";
+echo $_SERVER['SCRIPT_NAME'];
 ?>
 </body>
 </html>
