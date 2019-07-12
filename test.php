96e97458053f66db26ee28b22cf6bc18206e5bc8
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-12 16:53:39 +0530

    $GLOBALS

--- test.php
+++ test.php
@@ -3,15 +3,16 @@
 <body>
 
 <h1>My first PHP page</h1>
-<p>PHP Associative Arrays</p>
 <?php
-
-$age = array ("Peter" => "35", "Ben" => "37", "Joe" => "48");
-
-foreach ($age as $x => $x_value) {
-echo "Key=" .$x. ", Value=" .$x_value;
-echo "<br>";
+$x = 75; 
+$y = 25;
+ 
+function addition() { 
+    $GLOBALS['z'] = $GLOBALS['x'] + $GLOBALS['y']; 
 }
+ 
+addition(); 
+echo $z; 
 ?>
 </body>
 </html>
