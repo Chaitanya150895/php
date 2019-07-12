bdfc32d0b26c1e94a2fe28882bdd6752540fb010
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-12 15:54:51 +0530

    a function with two arguments ($fname and $year)

--- test.php
+++ test.php
@@ -5,15 +5,15 @@
 <h1>My first PHP page</h1>
 
 <?php
-function familyName($fname) {
-    echo "$fname Refsnes.<br>";
+function familyName($fname, $year) {
+    echo "$fname Refsnes, Born in $year <br>";
 }
 
-familyName("Jani");
-familyName("Hege");
-familyName("Stale");
-familyName("Kai Jim");
-familyName("Borge");
+familyName("Jani", "1945");
+familyName("Hege", 1998);
+familyName("Stale", "1979");
+familyName("Kai Jim", "1994");
+familyName("Borge", "1988");
 ?>
 </body>
 </html>
