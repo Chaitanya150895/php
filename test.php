2f46b6ec56fad7cc025fa380e86f35ceb7d3750d
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-12 13:50:12 +0530

    output "Have a good day!" if the current time (HOUR) is less than 20

--- test.php
+++ test.php
@@ -5,10 +5,11 @@
 <h1>My first PHP page</h1>
 
 <?php
-$x = array ("a" => "red,", "b" => "green,");
-$y = array ("c" => "blue,", "d" => "yellow");
+$t = date("H");
 
-var_dump($x !== $y);
+if ($t < "20") {
+    echo "Have a good day!";
+}
 ?>
 </body>
 </html>
