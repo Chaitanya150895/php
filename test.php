56d4bb1e80dc4d8388cefa19050a42cd56530f89
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-12 11:13:45 +0530

    creates a constant with a case-insensitive name

--- test.php
+++ test.php
@@ -5,9 +5,8 @@
 <h1>My first PHP page</h1>
 
 <?php
-// case-sensitive constant name
-define("GREETING", "Welcome to W3Schools.com!");
-echo GREETING;
+define("GREETING", "Welcome to W3Schools.com!", true);
+echo greeting;
 ?>
 </body>
 </html>
