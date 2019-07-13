cf38694b4faee7a1745918308b69e735627e0723
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-13 23:30:58 +0530

    to Check if Cookies are Enabled

--- test.php
+++ test.php
@@ -1,17 +1,16 @@
-<!DOCTYPE html>
 <?php
-// set the expiration date to one hour ago
-setcookie("user", "", time() - 3600);
+setcookie("test_cookie", "test", time() + 3600, '/');
 ?>
 <html>
 <body>
 
 <?php
-echo "Cookie 'user' is deleted.";
+if(count($_COOKIE) > 0) {
+    echo "Cookies are enabled.";
+} else {
+    echo "Cookies are disabled.";
+}
 ?>
-
-
-<p><strong>Note:</strong> You might have to reload the page to see the value of the cookie.</p>
 
 </body>
 </html>
