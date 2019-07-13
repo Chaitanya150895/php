6d05773aefdd51956cf1663a2b40c7942fc3c10a
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-13 23:29:06 +0530

    delete a cookie

--- test.php
+++ test.php
@@ -1,20 +1,15 @@
 <!DOCTYPE html>
 <?php
-$cookie_name = "user";
-$cookie_value = "John Doe";
-setcookie($cookie_name, $cookie_value, time() + (86400 * 30), "/"); // 86400 = 1 day
+// set the expiration date to one hour ago
+setcookie("user", "", time() - 3600);
 ?>
 <html>
 <body>
 
 <?php
-if(!isset($_COOKIE[$cookie_name])) {
-     echo "Cookie named '" . $cookie_name . "' is not set!";
-} else {
-     echo "Cookie '" . $cookie_name . "' is set!<br>";
-     echo "Value is: " . $_COOKIE[$cookie_name];
-}
+echo "Cookie 'user' is deleted.";
 ?>
+
 
 <p><strong>Note:</strong> You might have to reload the page to see the value of the cookie.</p>
 
