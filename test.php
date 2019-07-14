5403eafda4c792340a49c038cc45ea9e0144b1eb
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-14 07:08:52 +0530

    Filter Extension

--- test.php
+++ test.php
@@ -1,17 +1,29 @@
-<?php
-session_start();
-?>
 <!DOCTYPE html>
 <html>
+<head>
+<style>
+table, th, td {
+    border: 1px solid black;
+    border-collapse: collapse;
+}
+th, td {
+    padding: 5px;
+}
+</style>
+</head>
 <body>
 
-<?php
-// remove all session variables
-session_unset(); 
-
-// destroy the session 
-session_destroy(); 
-?>
+<table>
+  <tr>
+    <td>Filter Name</td>
+    <td>Filter ID</td>
+  </tr>
+  <?php
+  foreach (filter_list() as $id =>$filter) {
+      echo '<tr><td>' . $filter . '</td><td>' . filter_id($filter) . '</td></tr>';
+  }
+  ?>
+</table>
 
 </body>
 </html>
