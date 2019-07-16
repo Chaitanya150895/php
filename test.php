5755e7595d13472835936c35ff99cf5e050beb8b
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-16 17:59:18 +0530

    select data using MySQLi Object-oriented

--- test.php
+++ test.php
@@ -5,23 +5,24 @@ $password = "";
 $dbname = "myDB";
 
 // Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
+$conn = new mysqli($servername, $username, $password, $dbname);
 // Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
 $sql = "SELECT id, firstname, lastname FROM MyGuests";
-$result = mysqli_query($conn, $sql);
+$result = $conn->query($sql);
 
-if (mysqli_num_rows($result) > 0) {
+if ($result->num_rows > 0) {
+    echo "<table><tr><th>ID</th><th>Name</th></tr>";
     // output data of each row
-    while($row = mysqli_fetch_assoc($result)) {
-        echo "id: " . $row["id"]. " - Name: " . $row["firstname"]. " " . $row["lastname"]. "<br>";
+    while($row = $result->fetch_assoc()) {
+        echo "<tr><td>".$row["id"]."</td><td>".$row["firstname"]." ".$row["lastname"]."</td></tr>";
     }
+    echo "</table>";
 } else {
     echo "0 results";
 }
-
-mysqli_close($conn);
+$conn->close();
 ?>
