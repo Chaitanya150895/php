33bcb901099480708a907a5b78f985b1ee8e00f6
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-16 17:13:39 +0530

    new entry created using PDO

--- test.php
+++ test.php
@@ -4,21 +4,20 @@ $username = "root";
 $password = "";
 $dbname = "myDB";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('Jane', 'Doe', 'jane@example.com')";
-
-if (mysqli_query($conn, $sql)) {
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES ('Sumit', 'Tiwari', 'sumit@example.com')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
     echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
 
-mysqli_close($conn);
+$conn = null;
 ?>
