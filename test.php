647ad22e122403f34efbfca5ae97cd1baa9eb314
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-16 18:06:50 +0530

    deleting id - 2 using MySQLi Procedural

--- test.php
+++ test.php
@@ -5,20 +5,20 @@ $password = "";
 $dbname = "myDB";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-// sql to delete a record
-$sql = "DELETE FROM MyGuests WHERE id=3";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Record deleted successfully";
-} else {
-    echo "Error deleting record: " . $conn->error;
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
-$conn->close();
+// sql to delete a record
+$sql = "DELETE FROM MyGuests WHERE id=2";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record deleted successfully";
+} else {
+    echo "Error deleting record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
