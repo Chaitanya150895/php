d643d8adef6f78a8830f5cfc30f47b4187231bf3
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-16 17:10:02 +0530

    To add a new record to the "MyGuests" table using MySQLi Object-oriented

--- test.php
+++ test.php
@@ -2,30 +2,23 @@
 $servername = "localhost";
 $username = "root";
 $password = "";
-$dbname = "myDBPDO";
+$dbname = "myDB";
 
-try {
-    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
-    // set the PDO error mode to exception
-    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+// Create connection
+$conn = new mysqli($servername, $username, $password, $dbname);
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+} 
 
-    // sql to create table
-    $sql = "CREATE TABLE MyGuests (
-    id INT(6) UNSIGNED AUTO_INCREMENT PRIMARY KEY, 
-    firstname VARCHAR(30) NOT NULL,
-    lastname VARCHAR(30) NOT NULL,
-    email VARCHAR(50),
-    reg_date TIMESTAMP
-    )";
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('John', 'Doe', 'john@example.com')";
 
-    // use exec() because no results are returned
-    $conn->exec($sql);
-    echo "Table MyGuests created successfully";
-    }
-catch(PDOException $e)
-    {
-    echo $sql . "<br>" . $e->getMessage();
-    }
+if ($conn->query($sql) === TRUE) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
 
-$conn = null;
+$conn->close();
 ?>
