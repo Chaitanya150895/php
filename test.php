594e4a2be81167f06b5953c07279bc3e6d808e53
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-16 18:14:32 +0530

    updating the last name of the entry with id 7 MySQLi Procedural

--- test.php
+++ test.php
@@ -5,19 +5,19 @@ $password = "";
 $dbname = "myDB";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "UPDATE MyGuests SET lastname='Pande' WHERE id=4";
-
-if ($conn->query($sql) === TRUE) {
-    echo "Record updated successfully";
-} else {
-    echo "Error updating record: " . $conn->error;
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
-$conn->close();
+$sql = "UPDATE MyGuests SET lastname='Tiwari' WHERE id=7";
+
+if (mysqli_query($conn, $sql)) {
+    echo "Record updated successfully";
+} else {
+    echo "Error updating record: " . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
