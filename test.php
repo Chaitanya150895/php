a2fc0ab82b26314b32e8a89ca296f89d1dbd0031
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-16 17:11:46 +0530

    new entry created using the MySQLi Procedural

--- test.php
+++ test.php
@@ -5,20 +5,20 @@ $password = "";
 $dbname = "myDB";
 
 // Create connection
-$conn = new mysqli($servername, $username, $password, $dbname);
+$conn = mysqli_connect($servername, $username, $password, $dbname);
 // Check connection
-if ($conn->connect_error) {
-    die("Connection failed: " . $conn->connect_error);
-} 
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
-
-if ($conn->query($sql) === TRUE) {
-    echo "New record created successfully";
-} else {
-    echo "Error: " . $sql . "<br>" . $conn->error;
+if (!$conn) {
+    die("Connection failed: " . mysqli_connect_error());
 }
 
-$conn->close();
+$sql = "INSERT INTO MyGuests (firstname, lastname, email)
+VALUES ('Jane', 'Doe', 'jane@example.com')";
+
+if (mysqli_query($conn, $sql)) {
+    echo "New record created successfully";
+} else {
+    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
+}
+
+mysqli_close($conn);
 ?>
