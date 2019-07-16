8a80a7dd0b211588623ad7293e286fd1b2a6caff
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-16 17:20:39 +0530

    added one single line of code to retrieve the ID of the last inserted record. We also echo the last inserted ID using Example (PDO)

--- test.php
+++ test.php
@@ -4,22 +4,21 @@ $username = "root";
 $password = "";
 $dbname = "myDB";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
-
-$sql = "INSERT INTO MyGuests (firstname, lastname, email)
-VALUES ('John', 'Doe', 'john@example.com')";
-
-if (mysqli_query($conn, $sql)) {
-    $last_id = mysqli_insert_id($conn);
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $sql = "INSERT INTO MyGuests (firstname, lastname, email)
+    VALUES ('John', 'Doe', 'john@example.com')";
+    // use exec() because no results are returned
+    $conn->exec($sql);
+    $last_id = $conn->lastInsertId();
     echo "New record created successfully. Last inserted ID is: " . $last_id;
-} else {
-    echo "Error: " . $sql . "<br>" . mysqli_error($conn);
-}
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
 
-mysqli_close($conn);
+$conn = null;
 ?>
