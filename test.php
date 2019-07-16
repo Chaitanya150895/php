281d091e84ee26f4c91c1ceb2434d1cd335dd539
Chaitanya Sharma <chaitanya150895@gmail.com>
2019-07-16 18:20:06 +0530

    updating the data of the row with id = 9 using PDO

--- test.php
+++ test.php
@@ -4,20 +4,26 @@ $username = "root";
 $password = "";
 $dbname = "myDB";
 
-// Create connection
-$conn = mysqli_connect($servername, $username, $password, $dbname);
-// Check connection
-if (!$conn) {
-    die("Connection failed: " . mysqli_connect_error());
-}
+try {
+    $conn = new PDO("mysql:host=$servername;dbname=$dbname", $username, $password);
+    // set the PDO error mode to exception
+    $conn->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
 
-$sql = "UPDATE MyGuests SET lastname='Tiwari' WHERE id=7";
+    $sql = "UPDATE MyGuests SET lastname='Kulkarni' WHERE id=9";
 
-if (mysqli_query($conn, $sql)) {
-    echo "Record updated successfully";
-} else {
-    echo "Error updating record: " . mysqli_error($conn);
-}
+    // Prepare statement
+    $stmt = $conn->prepare($sql);
 
-mysqli_close($conn);
+    // execute the query
+    $stmt->execute();
+
+    // echo a message to say the UPDATE succeeded
+    echo $stmt->rowCount() . " records UPDATED successfully";
+    }
+catch(PDOException $e)
+    {
+    echo $sql . "<br>" . $e->getMessage();
+    }
+
+$conn = null;
 ?>
